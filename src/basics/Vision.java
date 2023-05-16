800df2418f09f2539cd1137e314b2e276b3cac86
anis <anis.siam@hotmail.com>
2023-05-16 21:55:40 +0300

    first commit

---
+++ src/basics/Vision.java
@@ -0,0 +1,26 @@
+package basics;
+
+public class Vision {
+    public static void main(String[] args) {
+
+        //System.out.println("Hello world!!"); // سطر برمجي لاغراض الطباعة
+       /* System.out.println("Anis Siam"); // سطر برمجي لاغراض الطباعة
+        System.out.println("Anis Siam"); // سطر برمجي لاغراض الطباعة
+
+        System.out.print("Anis");
+        System.out.print(" Siam");*/
+
+        System.out.println(1*1 + 5 *54);
+
+        int x=1;
+        //int x = 10; // error
+
+        System.out.println(x);
+
+        x = 10;
+        System.out.println(x);
+
+
+       // byte y = 128;
+    }
+}
