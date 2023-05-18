47bbed789da4367c21d2ddc28b4ecc792f3ebb1c
anis <anis.siam@hotmail.com>
2023-05-18 19:06:18 +0300

    18/May/2023 -Variables -DataType -Operators

--- src/basics/Vision.java
+++ src/basics/Vision.java
@@ -1,8 +1,89 @@
 package basics;
 
 public class Vision {
+    static int y;
+    static byte b;
+
+    static double qqq;
+    //System.out.println(0);
+
+    static boolean vvv;
+
     public static void main(String[] args) {
 
+int cccc;
+        int n1 = 5;
+        int n2 = 10;
+
+        int result = n1 + n2; //15
+        //       15 + 5 = 20
+        result = result + 5;//result += 5;
+
+
+        System.out.println(result);
+
+        double ddddd = result;
+        System.out.println(ddddd);
+        double result8 = 10 / 3.0;
+        System.out.println(result8);
+
+
+        int relsut1 = result + 5;
+
+        int result2 = (10 + 20 + 30) / 3;
+       // int qqqdsc = 30 / 0;
+        //System.out.println(qqqdsc);
+        System.out.println(result2);
+
+        System.out.println("The result is " + (result + 5));
+
+        System.out.println(5 + 5);
+
+
+        int myInt =50;
+        System.out.println("myInt " + myInt);
+
+        double myDouble = myInt;
+        System.out.println("myDouble " + myDouble);
+
+        byte myByte =(byte) myDouble;
+        System.out.println("myByte " + myByte);
+
+
+        byte tt =(byte) 129;
+        System.out.println(tt);
+
+        int zzzz = (int)10000000000000L;
+        System.out.println(zzzz);
+        double v = 5;
+        System.out.println(v);
+
+        int num1 =(int) 3.5;
+        System.out.println(num1);
+
+        char c = 'A';
+        System.out.println(c);
+        int xxx = 67 ;
+        System.out.println((char) xxx);
+
+        float ff = 50.9f;
+        float fff = (float) 50.9;
+
+
+        boolean isOpen = true;
+        System.out.println(isOpen);
+
+
+        System.out.println( "The value is " +  qqq  +" and "+ y);
+        System.out.println(0);
+
+        b = 0;
+        System.out.println(y);
+
+        int z = 0;
+        System.out.println(z);
+
+        y = 50;
         //System.out.println("Hello world!!"); // سطر برمجي لاغراض الطباعة
        /* System.out.println("Anis Siam"); // سطر برمجي لاغراض الطباعة
         System.out.println("Anis Siam"); // سطر برمجي لاغراض الطباعة
@@ -10,9 +91,9 @@ public class Vision {
         System.out.print("Anis");
         System.out.print(" Siam");*/
 
-        System.out.println(1*1 + 5 *54);
+        System.out.println(1 * 1 + 5 * 54);
 
-        int x=1;
+        int x = 1;
         //int x = 10; // error
 
         System.out.println(x);
@@ -22,6 +103,29 @@ public class Vision {
 
         //Hello
 
-       // byte y = 128;
+        // byte y = 128;
+
+
+        byte ww = 20;
+        System.out.println("The value is "+ ww);
+        long l = 10000000;
+        System.out.println(l+ " doller");
+
+        byte num = 50;
+        System.out.println("The value is " + num);
+
+        double dd = 0.0;
+        System.out.println(dd);
+
+
+        System.out.println(50.3);
+
+    }
+
+    // int x = 10;
+
+    public void nnn() {
+        // x = 50;
+        y = 10;
     }
 }
